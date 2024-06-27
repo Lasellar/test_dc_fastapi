4378970cf7cbd6937af99e0129c3358b9bf3296d
Maksim Polushin <btnchsky.sad@mail.ru>
2024-06-27 03:15:25 +0300

    убрал старые модели юзера

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
-from database import SessionLocal, engine, Base
-from routers import users as users_router
+from database import engine, Base
 from routers import tasks as tasks_router
 from auth.routers import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
-app.include_router(users_router.router, prefix='/users', tags=['user'])
 app.include_router(tasks_router.router, prefix='/tasks', tags=['task'])
 app.include_router(auth_router, prefix='/access', tags=['auth'])
 
